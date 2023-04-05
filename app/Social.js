19c7a3ce6054fe1cc61261748100dd3cf04f3b94
Peter1907 <peter.nady.1408@gmail.com>
2023-04-05 13:57:55 +0200

    Update social links to open in a new tab

--- app/Social.js
+++ app/Social.js
@@ -5,25 +5,41 @@ export default function Social() {
   return (
     <section className="fixed hidden z-20 text-gray-400 md:block">
       <div className="links flex flex-col items-center gap-4 fixed bottom-0 left-6">
-        <a href="https://github.com/Peter1907">
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://github.com/Peter1907"
+        >
           <FontAwesomeIcon
             icon={faGithub}
             className="w-5 hover:mb-1 hover:-mt-1 hover:text-aqua/80 transition-all duration-200"
           />
         </a>
-        <a href="https://www.linkedin.com/in/peter-nady-beshara">
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.linkedin.com/in/peter-nady-beshara"
+        >
           <FontAwesomeIcon
             icon={faLinkedinIn}
             className="w-5 hover:mb-1 hover:-mt-1 hover:text-aqua/80 transition-all duration-200"
           />
         </a>
-        <a href="https://wellfound.com/u/peter-beshara">
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://wellfound.com/u/peter-beshara"
+        >
           <FontAwesomeIcon
             icon={faAngellist}
             className="w-5 hover:mb-1 hover:-mt-1 hover:text-aqua/80 transition-all duration-200"
           />
         </a>
-        <a href="https://medium.com/@peter.beshara">
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://medium.com/@peter.beshara"
+        >
           <FontAwesomeIcon
             icon={faMedium}
             className="w-5 hover:mb-1 hover:-mt-1 hover:text-aqua/80 transition-all duration-200"
